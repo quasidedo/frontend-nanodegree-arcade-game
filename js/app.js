e2f1e0edd47307b0a4f97688b34dd053ad5c6585
Luis Amez <Luis Amez>
2016-08-26 21:42:44 +0200

    Update app.js
    Defined basic functions for enemy and player.

--- js/app.js
+++ js/app.js
@@ -24,13 +24,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+}
 
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var enemy1 = new Enemy;
+var allEnemies = [enemy1];
 // Place the player object in a variable called player
-
-
+var player = new Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
